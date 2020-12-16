cca3862c9306700e0f8c408401326907bc36b7c9
jvlsg <jvlsg@users.noreply.github.com>
2020-12-16 18:00:30 -0300

    Began str parsing

--- src/lib.rs
+++ src/lib.rs
@@ -2,33 +2,26 @@ use rand::Rng;
 use std::str;
 use std::fmt;
 use std::num;
+use regex::Regex;
 
-
-pub struct DiceRollResult{
-    base_value: i64, //Original value
-    mod_value: i64, //Modified Value: Original + inc - dec
-    is_success: bool
-}
 
 pub enum DiceRollType{
     Sum,
     Pool
-    //Sum(Option<DiceRollResult>),
-    //Pool(Option< {results: Vec<DiceRollResult>, num_success: i64} >) //store Option<{results: Vec<DiceRollResult>, num_success: i64 }>
 }
 
 pub struct DiceRoll{
-    dice_type: i64,
     dice_qty: i64,
+    dice_type: i64,
     inc: i64, //Increment
     dec: i64, //Decrement
-    tn: i64,   //Target Number
+    tn: i64,   //Target Number. Defaults to 0
     roll_type: DiceRollType,
-    roll_results: Vec<DiceRollResult>
+    roll_results: Vec<i64>,
+    final_result: i64 //
 }
 
 impl DiceRoll {
-
     pub fn new(dice_qty: i64,
                dice_type: i64,
                inc: i64,
@@ -43,38 +36,32 @@ impl DiceRoll {
             dec,
             tn,
             roll_type,
-            roll_results : vec![]
+            roll_results : vec![],
+            final_result : 0 //Either the modified sum or the number of dice pool Successes
         }
     }
 
     pub fn roll_dice(&mut self) {
+        //Reset variables
+        self.roll_results.clear();
+        self.final_result = 0;
+
         match self.roll_type {
             DiceRollType::Sum => {
-                let mut base_value:i64 = 0;
                 for _i in 0..self.dice_qty {
-                    base_value += rand::thread_rng().gen_range(1, self.dice_type+1);
+                    self.roll_results.push(rand::thread_rng().gen_range(1, self.dice_type+1));
                 }
-                let mod_value = base_value + self.inc - self.dec;
-                let is_success = if mod_value >= self.tn {true} else {false};
-                self.roll_results.push(DiceRollResult{
-                    base_value,
-                    mod_value,
-                    is_success
-                });
+                self.final_result = self.roll_results.iter().sum::<i64>() + self.inc - self.dec;
             }
             DiceRollType::Pool => {
-            //TODO: reset roll_results if not empty
-                for _i in 0..self.dice_qty {
-                    let base_value = rand::thread_rng().gen_range(1, self.dice_type+1);
-                    let mod_value = base_value + self.inc - self.dec;
-                    let is_success = if mod_value + self.inc - self.dec >= self.tn {
-                        true
-                    } else {false};
-                    self.roll_results.push(DiceRollResult{
-                        base_value,
-                        mod_value,
-                        is_success
-                    });
+                let mut success_count: i64 = 0;
+                for _i in 0..(self.dice_qty + self.inc - self.dec) { //Number of dice is modified
+                    let r =rand::thread_rng().gen_range(1, self.dice_type+1);
+                    if r  >= self.tn {
+                        success_count+=1;
+                    }
+                    self.roll_results.push(r);
+                    self.final_result = success_count;
                 }
             }
         }
@@ -85,16 +72,18 @@ impl DiceRoll {
 impl str::FromStr for DiceRoll {
     type Err = num::ParseIntError;
     fn from_str(s: &str) -> Result<Self,Self::Err> {
-        let v: Vec<&str> = s.split('d').collect();
+        let re = Regex::new(r"^(?P<dice_qty>\d+)[d|D](?P<dice_type>\d+)(?P<mods>(?:\+\d+|-\d+)+)?(?:#(?P<tn>\d+))?$").unwrap();
+        for cap in re.captures_iter(s) {
+            println!("{:?}",&cap);
+        }
 
-        let dice_qty = v[0].parse::<i64>()?;
         let mut inc = 0;
         let mut dec = 0;
         let mut tn = 0;
         let dice_type = 0;
         let roll_type = DiceRollType::Sum;
         Ok(DiceRoll::new(
-            dice_qty,
+            1,
             dice_type,
             inc,
             dec,
@@ -106,32 +95,33 @@ impl str::FromStr for DiceRoll {
 
 impl fmt::Display for DiceRoll {
     fn fmt(&self,f: &mut fmt::Formatter<'_>) -> fmt::Result {
-
-//        String modifier_info = if self.inc == 0 && self.dec == 0 { 
-//            "".to_string() 
-//        } else {
-//            format!("+{} -{} = {}",self.inc,self.dec)
-//        };
         match self.roll_type {
             DiceRollType::Sum => {
-                let result=&self.roll_results[0];
-                let mod_str = if self.tn > 0 {format!(" ➔ {}",result.mod_value)} else {"".to_string()};
+                let base_result:i64 = self.roll_results.iter().sum();
+                let mod_str = if self.tn > 0 {format!(" ➔ {}",self.final_result)} else {"".to_string()};
                 if self.tn > 0 {
-                   let success_str = if result.mod_value > self.tn {" ✓ "} else {" ✕ "};
-                    write!(f,"[{}{}{}]",result.base_value,mod_str,success_str)
+                   let success_str = if self.final_result > self.tn {" ✓ "} else {" ✕ "};
+                    write!(f,"[{}{}{}]",base_result,mod_str,success_str)
                 }
                 else{
                     write!(f,"[{}{}]",
-                           self.roll_results[0].base_value,
+                           base_result,
                            mod_str,)
                 }
             }
             DiceRollType::Pool => {
-                let mut x = String::from("");
-                for result in &self.roll_results{
-                    x = format!("[{}] {}" ,result.mod_value,x);
+                let mut results_str = String::from("");
+                let success_str = if self.tn>0 {
+                    let word = if self.final_result == 1 { "Success" } else { "Successes" };
+                    format!("{} {}",self.final_result,word)
+                } else {
+                    "".to_string()
+                };
+                
+                for die_result in &self.roll_results{
+                    results_str = format!("{} {}" ,die_result,results_str);
                 }
-                write!(f,"{}",x)
+                write!(f,"[{}| {}]",results_str,success_str)
             }
         }
     }
@@ -140,7 +130,11 @@ impl fmt::Display for DiceRoll {
 // TODO to use in verbose mode impl fmt::Debug for DiceRoll {}
 //impl fmt::Debug for DiceRoll {
 //    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//
+//        String modifier_info = if self.inc == 0 && self.dec == 0 { 
+//            "".to_string() 
+//        } else {
+//            format!("+{} -{} = {}",self.inc,self.dec)
+//        };
 //    }
 //}
 
@@ -164,14 +158,24 @@ mod tests {
 
     #[test]
     fn roll_pool(){
-        let mut x = DiceRoll::new(3,6,4,2,11,DiceRollType::Pool);
+        let mut x = DiceRoll::new(3,6,4,2,4,DiceRollType::Pool);
         x.roll_dice();
         println!("{}",x);
     }
 
     #[test]
+    fn re_play(){
+        let re = Regex::new(r"^.*(a).*$").unwrap();
+        for cap in re.captures_iter("daad") {
+            println!("{:?}",&cap);
+        }
+    }
+
+    #[test]
     fn roll_from_str(){
-        //let x = DiceRoll::from_str("3d6+12-1-5#12");
-        let x = "3d6+12-1-5#10".parse::<DiceRoll>();
+        let x = "3D6-3-3+1#5".parse::<DiceRoll>();
+        let x = "31d632+12-3+1#10".parse::<DiceRoll>();
+        let y = "15d16".parse::<DiceRoll>();
+        let z = "1d3#2".parse::<DiceRoll>();
     }
 }
